2f2c95a9d98e2dc1cf5656f3e6778912eba309f0
Brian Dawkins <bkdawkins@gmail.com>
2018-03-17 21:29:49 -0700

    final index.html final style.css final game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,75 +2,64 @@
 
 $(document).ready(function() {
 
-var gameNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-var wins = 0;
-var losses = 0;
-var pinkCrystal = Math.floor(Math.random() * (12 - 1 + 1)) + 12;
-var greenCrystal = Math.floor(Math.random() * (12 - 1 + 1)) + 12;
-var yellowCrystal = Math.floor(Math.random() * (12 - 1 + 1)) + 12;
-var silverCrystal = Math.floor(Math.random() * (12 - 1 + 1)) + 12;
-var totalScore = [""];
-var randomNumber = [""];
-$(".randomNumber").append(gameNumber); 
+    var gameNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    var pinkCrystal = Math.floor(Math.random() * (12 - 1 + 1)) + 12;
+    var greenCrystal = Math.floor(Math.random() * (12 - 1 + 1)) + 12;
+    var yellowCrystal = Math.floor(Math.random() * (12 - 1 + 1)) + 12;
+    var silverCrystal = Math.floor(Math.random() * (12 - 1 + 1)) + 12;
+    var totalScore = 0;
+    var wins = 0;
+    var losses = 0;
+    var randomNumber = [""];
+    $(".randomNumber").append(gameNumber); 
 
-// function restartGame(){
-//     gameNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-//     pinkCrystal = Math.floor(Math.random() * (12 - 1 + 1)) + 12;
-//     greenCrystal = Math.floor(Math.random() * (12 - 1 + 1)) + 12;
-//     yellowCrystal = Math.floor(Math.random() * (12 - 1 + 1)) + 12;
-//     silverCrystal = Math.floor(Math.random() * (12 - 1 + 1)) + 12;
-//     totalScore = [];
-//     console.log("game is restarted");
-// }
-
-$(document).ready(function() {
+function restartGame(){
     gameNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
     pinkCrystal = Math.floor(Math.random() * (12 - 1 + 1)) + 12;
     greenCrystal = Math.floor(Math.random() * (12 - 1 + 1)) + 12;
     yellowCrystal = Math.floor(Math.random() * (12 - 1 + 1)) + 12;
     silverCrystal = Math.floor(Math.random() * (12 - 1 + 1)) + 12;
-    totalScore = 0;
-    wins = 0;
-    losses = 0;
-});
-$(document).on("click", function(){
-    // if (totalScore === gameNumber){
-    //     wins++;
-    // } 
-    if (totalScore > gameNumber){
-        console.log("loser");
-        
+    $(".totalScore").html(0);
+    var randomNumber = [""];
+    $(".randomNumber").html(gameNumber);
     }
-
-})
-    $("#pink-crystal").on("click", function() {
-     totalScore += pinkCrystal
-    $(".totalScore").html(totalScore);
-    })
-   $("#green-crystal").on("click", function() {
-    totalScore += greenCrystal
-   $(".totalScore").html(totalScore);
-   })
-   $("#yellow-crystal").on("click", function() {
-    totalScore += yellowCrystal
-   $(".totalScore").html(totalScore);
-   })
-   $("#silver-crystal").on("click", function() {
-    totalScore += silverCrystal
-   $(".totalScore").html(totalScore);
-    })
     
-
-
-
+$(document).ready(function() {
+    pinkCrystal = Math.floor(Math.random() * (12 - 1 + 1)) + 12;
+    greenCrystal = Math.floor(Math.random() * (12 - 1 + 1)) + 12;
+    yellowCrystal = Math.floor(Math.random() * (12 - 1 + 1)) + 12;
+    silverCrystal = Math.floor(Math.random() * (12 - 1 + 1)) + 12;
 });
-
-
-
-        // function add(a, b) {
-        //     return a + b;
-        // }
-        
-        // console.log(sum);
+  
+$("#pink-crystal").on("click", function() {
+    totalScore += pinkCrystal
+    $(".totalScore").html(totalScore);
+})
+$("#green-crystal").on("click", function() {
+    totalScore += greenCrystal
+    $(".totalScore").html(totalScore);
+})
+$("#yellow-crystal").on("click", function() {
+    totalScore += yellowCrystal
+    $(".totalScore").html(totalScore);
+})
+$("#silver-crystal").on("click", function() {
+    totalScore += silverCrystal
+    $(".totalScore").html(totalScore);
+})
+$(document).on("click", function() {
+    if (totalScore > gameNumber) {
+    losses++;
+    $("#losses").html("<p> Losses" + ":" + losses + "</p>");
+    restartGame();
+}
+if (totalScore === gameNumber) {
+    wins++;
+    $("#wins").html("<p> Wins" + ":" + wins + "</p>");
+    restartGame();
+}
+})
+});
 
 /* End Javascript*/
+    
